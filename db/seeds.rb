8bb4fd2853b2811947437b481b3208feb32a93c6
Joshua <joycejosh7@gmail.com>
2021-03-06 15:31:50 -0500

    migrated and seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+mom = Creator.create(name:"Mom")
+dad = Creator.create(name:"Dad")
+child = Creator.create(name:"Child")
+
+Activity.create(title:"Swim", description:"at the beach", author_id:mom.id)
+Activity.create(title:"bike", description:"from home", author_id:dad.id)
+Activity.create(title:"fly kites", description:"at the park", author_id:child.id)

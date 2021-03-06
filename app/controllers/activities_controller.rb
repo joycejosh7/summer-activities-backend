90c57876e3a4e7d7f138d987fc8b97643ccdd0bb
Joshua <joycejosh7@gmail.com>
2021-03-06 15:39:38 -0500

    filled out activity controller

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,43 @@
 class ActivitiesController < ApplicationController
+    def index
+        activity = Activity.all 
+        # render json: activity
+        render json: Activity.arr_to_json
+        #json behind the scenes turns your hash into a string
+        #http can only send/receive/read information in STRING format
+
+    end 
+
+    
+    def show
+        activity = Activity.find(params[:id])
+        render json: activity.instance_to_json 
+    end 
+
+    def create
+        activity = Activity.new(activity_params)
+        creator = Creator.find_or_create_by(name: params[:creator])
+        activity.creator_id =  creator.id
+        if activity.save
+            render json: activity.instance_to_json
+        else 
+            render json: activity.errors, status: :unprocessable_entity
+        end
+    end 
+
+    def update
+        activity = Activity.find_by(id:params[:id])
+        if activity.save
+            render json: activity.instance_to_json
+        else 
+            render json: activity.errors, status: :unprocessable_entity
+        end
+    end 
+
+
+    private 
+
+    def activity_params
+        params.require(:activity).permit(:title,:content,:creator_id)
+    end
 end

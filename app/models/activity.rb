94c461d572567f7aa1711d8193419018cbee959e
Joshua <joycejosh7@gmail.com>
2021-03-06 15:42:58 -0500

    created methods for activities

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,23 @@
 class Activity < ApplicationRecord
     belongs_to :creator
+
+    #class method that handles arr of activities
+    def self.arr_to_json
+        self.all.map do |a|
+            a.instance_to_json
+        end 
+    end 
+
+    # instance method that handles one specific activity
+    def instance_to_json
+        { 
+            id: self.id,
+            title: self.title,
+            content: self.content,
+            author: {
+                id: self.author.id,
+                name: self.author.name
+             }
+         }
+    end 
 end
